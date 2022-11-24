3e3cb17cce0b6a192426086f5e80f83f80e6ffc8
marcinserafin5 <36891799+marcinserafin5@users.noreply.github.com>
2022-11-24 22:45:45 +0100

    initial UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const atmButtons = [
+    { label: "1", value: "1", style: "atmButton" },
+    { label: "2", value: "2", style: "atmButton" },
+    { label: "3", value: "3", style: "atmButton" },
+    { label: "CANCEL", value: "cancel", style: "bigAtmButton yellow" },
+
+    { label: "4", value: "4", style: "atmButton" },
+    { label: "5", value: "5", style: "atmButton" },
+    { label: "6", value: "6", style: "atmButton" },
+    { label: "CLEAR", value: "clear", style: "bigAtmButton red" },
+
+    { label: "7", value: "7", style: "atmButton" },
+    { label: "8", value: "8", style: "atmButton" },
+    { label: "9", value: "9", style: "atmButton" },
+    { label: "ENTER", value: "enter", style: "bigAtmButton green" },
+
+    { label: "", value: "", style: "atmButton" },
+    { label: "0", value: "0", style: "atmButton" },
+    { label: "", value: "", style: "atmButton" },
+    { label: "", value: "", style: "bigAtmButton" },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <div className="atm">
+          <span className="App-header">ATM</span>
+          <div className="displayContainer">
+            <div className="display">
+              <div className="textContainer">
+                <span>Account Balance: 23213PLN</span>
+                
+                <span>To withdraw</span>
+              </div>
+              <div className="buttonContainer">
+                <button>WITHDRAW</button>
+                <button>DEPOSIT</button>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{
+              width: "53rem",
+              height: "43rem",
+              justifySelf: "center",
+              margin: 0,
+              alignSelf: "center",
+              display: "flex",
+            }}
+          >
+            <div className="atmKeyboard">
+              {atmButtons.map((button) => {
+                return (
+                  <button
+                    onClick={() => console.log(button.value)}
+                    className={button.style}
+                  >
+                    {button.label}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
